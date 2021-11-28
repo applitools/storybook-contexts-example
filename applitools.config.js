bcc8169ce93a26119b3dd82143d01687c1dc56aa
Amit Zur <amit.zur@applitools.com>
2021-11-28 08:10:01 +0200

    fix config file

--- applitools.config.js
+++ applitools.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   testConcurrency: 20,
+  batchName: 'addon-contexts demo',
   queryParams: [
     {name: 'contexts', value: 'Themes=Light Theme,Language=English'},
     {name: 'contexts', value: 'Themes=Dark Theme,Language=English'},
-    {name: 'contexts', value: 'Themes=Light Theme,Language=German'},
-    {name: 'contexts', value: 'Themes=Dark Theme,Language=German'},
+    {name: 'contexts', value: 'Themes=Light Theme,Language=Japanese'},
+    {name: 'contexts', value: 'Themes=Dark Theme,Language=Japanese'},
   ]
 }
